49f648b04824db2b1873dc03413b7fd92041d73a
RajanPatel098 <106442470+RajanPatel098@users.noreply.github.com>
2023-03-29 12:08:51 -0500

    Working
    have to solve add song to playlist still

--- playlist-app/forms.py
+++ playlist-app/forms.py
@@ -1,19 +1,35 @@
 """Forms for playlist app."""
 
-from wtforms import SelectField
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired
 from flask_wtf import FlaskForm
 
 
 class PlaylistForm(FlaskForm):
     """Form for adding playlists."""
-
     # Add the necessary code to use this form
+    name = StringField(
+        "Playlist Name",
+        validators=[InputRequired()]
+    )
+    description = StringField(
+        "Description",
+        validators=[InputRequired()]
+    )
 
 
 class SongForm(FlaskForm):
     """Form for adding songs."""
-
     # Add the necessary code to use this form
+    title = StringField(
+        "Title",
+        validators = [InputRequired()]
+    )
+    artist = StringField(
+        "Artist",
+        validators=[InputRequired()]
+    )
+
 
 
 # DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
@@ -21,3 +37,5 @@ class NewSongForPlaylistForm(FlaskForm):
     """Form for adding a song to playlist."""
 
     song = SelectField('Song To Add', coerce=int)
+
+
